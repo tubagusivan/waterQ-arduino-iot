59b52e5af4d5bd335c5411c75ab7ac5271ca9c82
tubagusivan <ivanbudiman.tb@gmail.com>
2020-02-17 11:56:34 +0700

    Updates

---
+++ precache-manifest.46b9df087849ad61091f2c5cb81aa41e.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "38839638703c5781ef565584e44d8618",
+    "url": "/water-Q/index.html"
+  },
+  {
+    "revision": "25135a0b739bb8a5ae81",
+    "url": "/water-Q/static/css/main.33aa24b8.chunk.css"
+  },
+  {
+    "revision": "4aeb62949a0539ecd32a",
+    "url": "/water-Q/static/js/2.18e8d942.chunk.js"
+  },
+  {
+    "revision": "170392c3acad6bbffec743442b1d0ac3",
+    "url": "/water-Q/static/js/2.18e8d942.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "25135a0b739bb8a5ae81",
+    "url": "/water-Q/static/js/main.5c8cbaa3.chunk.js"
+  },
+  {
+    "revision": "a58c6bce190325347f6f",
+    "url": "/water-Q/static/js/runtime-main.709bcaa6.js"
+  },
+  {
+    "revision": "1e9b8f1b63d868546620180d8c94d856",
+    "url": "/water-Q/static/media/jernih.1e9b8f1b.png"
+  },
+  {
+    "revision": "d9dabec641a94a2b89d2fa7577a610b4",
+    "url": "/water-Q/static/media/kotor.d9dabec6.png"
+  }
+]);
